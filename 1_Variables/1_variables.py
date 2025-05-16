ce47eea7d7f0985cb3a4bea3e9d377b687099692
DigiBronze <chenyu1160644409@gmail.com>
2025-05-16 11:41:05 +0800

    1_variables.py

--- 1_Variables/1_variables.py
+++ 1_Variables/1_variables.py
@@ -4,5 +4,21 @@ groceries = 3000
 print(bank_loan)
 total = bank_loan + credit_card + groceries
 
+name = 'jingjing'
+interest = 'read a book'
+career = student
+pritn('hello',name,interest,career)
+
+print(type(bank_loan))
+
+#boolean
+lear_python = True
+lear_fortran = False
+
+
+
+
+
+
 
 
